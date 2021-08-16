b98112d295830c6b899970723ce9b65993f4da08
Rodolpho Alves <rrpa15@live.com>
2021-08-16 15:37:21 -0300

    setup

---
+++ src/main/java/zup/proposta/rodolpho/RodolphoApplication.java
@@ -0,0 +1,13 @@
+package zup.proposta.rodolpho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodolphoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodolphoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zup/proposta/rodolpho/RodolphoApplicationTests.java
@@ -0,0 +1,13 @@
+package zup.proposta.rodolpho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodolphoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
